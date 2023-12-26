2662f29a8e4bf59051ce75383dd22599d3561fc8
drarroyo <ivan.arroyopozo@outlook.com>
2023-12-26 17:42:48 +0100

    operational version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # webtensites
+
+This project is a theoretical-practical test
+To carry it out, a script is requested that performs the following tasks:
+
+- Save the content of 10 random websites index pages to 10 files
+- Count number of matching lines with the string “href=” in each of the 10 files
+- Save result of “href=” matches in new 10 files, using logical naming convention Delete the newly created 10 files with results
+- Measure execution time
---
+++ save_and_count.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# List of websites 
+websites=( 
+  "www.savanamed.com" 
+  "www.github.com"
+	"www.stackoverflow.com"
+	"www.coursera.org"
+	"www.edx.org"
+	"www.khanacademy.org"
+	"www.skillshare.com"
+	"www.udemy.com"
+	"www.trello.com"
+	"www.atlassian.com"
+	"www.confluence.com"
+	"www.basecamp.com"
+	"www.asana.com"
+	"www.notion.so"
+	"www.evernote.com"
+	"www.adobe.com"
+	"www.sketchapp.com"
+	"www.figma.com"
+	"www.gimp.org"
+	"www.photopea.com"
+	"www.canva.com"
+	"www.inkscape.org"
+	"www.scrivenerapp.com"
+	"www.evernote.com"
+	"www.onenote.com"
+	"www.notion.so"
+	"www.pinterest.com"
+	"www.behance.net"
+	"www.dribble.com"
+	"www.upwork.com"
+	"www.fiverr.com"
+	"www.freelancer.com"
+	"www.indeed.com"
+	"www.monster.com"
+	"www.linkedin.com"
+	"www.glassdoor.com"
+	"www.quora.com"
+	"www.medium.com"
+	"www.blogger.com"
+	"www.wordpress.com"
+	"www.squarespace.com"
+	"www.wix.com"
+	"www.weebly.com"
+	"www.spotify.com"
+	"www.applemusic.com"
+	"www.amazon.com"
+	"www.netflix.com"
+	"www.primevideo.com"
+	"www.disneyplus.com"
+	"www.hbomax.com"
+	"www.twitch.tv"
+	"www.pandora.com"
+	"www.last.fm"
+	"www.google.com"
+	"www.soundcloud.com"
+	"www.youtube.com"
+)
+
+
+# Obtain array length
+length=${#websites[@]}
+echo "Array sites length: "$length
+
+# Generate 10 random sites
+sites=$(shuf -i 0-$length -n 10)
+echo $sites
+
+# Create 10 files to store the content of the index pages
+#for url in "${urls[@]}"
+for domains in $sites
+do
+   #echo "${domains}.index.html" > "${domains}.html"
+   # cur -L Follow redirects
+   #curl -L ${websites[$domains]} > "${websites[$domains]}.html"
+   curl -L ${websites[$domains].index.html} > "${websites[$domains]}.html"
+   echo "${websites[$domains]}"
+done
+
+# Count the number of lines matching the "href=" string
+for filename in *.html
+do
+  count=$(grep -c "href=" "${filename}")
+  echo "The file ${filename} contains ${count} matches."
+
+  new_filename=$(echo "${filename}" | sed 's/\.html/-hrefs.txt/')
+  echo "${count}" > "${new_filename}"
+done
+
+# Delete the 10 newly created result files
+for textfile in *-hrefs.txt
+do
+  rm "${textfile}"
+done
+
+# Measure the runtime
+end=$(date +%s)
+runtime=$((end-start))
+echo "The script ran in ${runtime} seconds."
+
